a3ad822e6d524c4e9422ec93c3ba04734d7ed735
Jining Liu <jiningdev@gmail.com>
2025-05-27 23:26:15 -0500

    bun support + added continuous scanning and interval setting

--- src/lib/components/ScanQRCode.svelte
+++ src/lib/components/ScanQRCode.svelte
@@ -1,109 +1,109 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import jsQR from "jsqr";
+  import { onMount } from "svelte";
+  import jsQR from "jsqr";
 
-    type optionsType = {
-        onPermissionError?: Function;
-        onResulted?: Function;
-    };
+  type optionsType = {
+    continuous?: boolean;
+    interval?: number;
+    onPermissionError?: Function;
+    onResulted?: Function;
+  };
 
-    export let scanResult: string = "";
-    export let enableQRCodeReaderButton: boolean;
-    export let options: optionsType;
+  export let scanResult: string = "";
+  export let enableQRCodeReaderButton: boolean;
+  export let options: optionsType;
 
-    let video: any;
-    let canvas: any;
+  let video: any;
+  let canvas: any;
 
-    onMount(() => {
-        requestCamera();
-    });
+  onMount(() => {
+    requestCamera();
+  });
 
-    function requestCamera() {
-        navigator.mediaDevices
-            .getUserMedia({
-                audio: false,
-                video: {
-                    facingMode: "environment",
-                },
-            })
-            .then((userStream) => {
-                video.srcObject = userStream;
-                video.setAttribute("playsinline", true);
-                video.play();
-                startScan();
-            })
-            .catch((err) => {
-                if (options?.onPermissionError != undefined) {
-                    options.onPermissionError();
-                } else {
-                    alert(err);
-                }
-            });
+  function requestCamera() {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: {
+          facingMode: "environment",
+        },
+      })
+      .then((userStream) => {
+        video.srcObject = userStream;
+        video.setAttribute("playsinline", true);
+        video.play();
+        startScan();
+      })
+      .catch((err) => {
+        if (options?.onPermissionError != undefined) {
+          options.onPermissionError();
+        } else {
+          alert(err);
+        }
+      });
+  }
+
+  function startScan() {
+    const context = canvas.getContext("2d", { willReadFrequently: true });
+    const { width, height } = canvas;
+
+    context.drawImage(video, 0, 0, width, height);
+
+    const imageData = context.getImageData(0, 0, width, height);
+    const qrCode = jsQR(imageData.data, width, height);
+
+    if (qrCode) {
+      scanResult = qrCode.data;
+      if (options.onResulted != undefined) {
+        options.onResulted(qrCode.data);
+      } else {
+        alert(qrCode.data);
+      }
+
+      if (options.continuous == true) {
+        setTimeout(startScan, options.interval || 500);
+      }
+    } else {
+      setTimeout(startScan, options.interval || 500);
     }
+  }
 
-    function startScan() {
-        const context = canvas.getContext("2d", { willReadFrequently: true });
-        const { width, height } = canvas;
+  function onCanPlay() {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    startScan();
+  }
 
-        context.drawImage(video, 0, 0, width, height);
-
-        const imageData = context.getImageData(0, 0, width, height);
-        const qrCode = jsQR(imageData.data, width, height);
-
-        if (qrCode) {
+  function qrcodeReader(e: any) {
+    try {
+      let file = e.target.files[0];
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (e: any) => {
+        let img = new Image();
+        img.src = e.target.result;
+        img.onload = () => {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          const context = canvas.getContext("2d");
+          context.imageSmoothingEnabled = false;
+          context.drawImage(img, 0, 0);
+          const imageData = context.getImageData(0, 0, img.width, img.height);
+          const qrCode = jsQR(imageData.data, img.width, img.height);
+          if (qrCode) {
             scanResult = qrCode.data;
             if (options?.onResulted != undefined) {
-                options.onResulted(qrCode.data);
+              options.onResulted(qrCode.data);
             } else {
-                alert(qrCode.data);
-                setTimeout(startScan, 1000);
+              alert(qrCode.data);
             }
-        } else {
-            setTimeout(startScan, 500);
-        }
+          }
+        };
+      };
+    } catch (error) {
+      console.error(error);
     }
-
-    function onCanPlay() {
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        startScan();
-    }
-
-    function qrcodeReader(e: any) {
-        try {
-            let file = e.target.files[0];
-            let reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onload = (e: any) => {
-                let img = new Image();
-                img.src = e.target.result;
-                img.onload = () => {
-                    canvas.width = img.width;
-                    canvas.height = img.height;
-                    const context = canvas.getContext("2d");
-                    context.imageSmoothingEnabled = false;
-                    context.drawImage(img, 0, 0);
-                    const imageData = context.getImageData(
-                        0,
-                        0,
-                        img.width,
-                        img.height
-                    );
-                    const qrCode = jsQR(imageData.data, img.width, img.height);
-                    if (qrCode) {
-                        scanResult = qrCode.data;
-                        if (options?.onResulted != undefined) {
-                            options.onResulted(qrCode.data);
-                        } else {
-                            alert(qrCode.data);
-                        }
-                    }
-                };
-            };
-        } catch (error) {
-            console.error(error);
-        }
-    }
+  }
 </script>
 
 <canvas bind:this={canvas} />
@@ -111,21 +111,21 @@
 <video on:canplay={onCanPlay} bind:this={video} />
 
 {#if enableQRCodeReaderButton}
-    <input
-        class="image-qrcode-input"
-        type="file"
-        id="upload-qrcode-image"
-        accept=".jpg, .jpeg, .png"
-        on:change={(e) => qrcodeReader(e)}
-    />
+  <input
+    class="image-qrcode-input"
+    type="file"
+    id="upload-qrcode-image"
+    accept=".jpg, .jpeg, .png"
+    on:change={(e) => qrcodeReader(e)}
+  />
 {/if}
 
 <style>
-    canvas {
-        display: none;
-    }
+  canvas {
+    display: none;
+  }
 
-    .image-qrcode-input {
-        margin-top: 20px;
-    }
+  .image-qrcode-input {
+    margin-top: 20px;
+  }
 </style>
